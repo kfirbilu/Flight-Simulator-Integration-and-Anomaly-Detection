9345d76d88708c4a3408cc17b7167cae6b0f47cb
kfirbilu <kfirbilu@gmail.com>
2021-04-29 19:52:48 +0300

    Moudle files

--- src/View/ButtonsController.java
+++ src/View/ButtonsController.java
@@ -1,4 +1,23 @@
 package View;
 
-public class ButtonsController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ButtonsController  {
+
+    @FXML private ChoiceBox choiceBox;
+
+
+
+
+
+
+
+
+
+
 }
