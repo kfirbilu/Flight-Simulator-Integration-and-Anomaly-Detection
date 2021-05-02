076644a62f6436ea7076348ae9b3219eae40d4a8
kfirbilu <kfirbilu@gmail.com>
2021-05-02 20:40:31 +0300

    open xml file button

--- src/View/ButtonsController.java
+++ src/View/ButtonsController.java
@@ -2,6 +2,7 @@ package View;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.DirectoryChooser;
@@ -30,21 +31,57 @@ public class ButtonsController implements Initializable {
 
         FileChooser fileChooser = new FileChooser();
 
-        fileChooser.setTitle("Open XML file");
+        fileChooser.setTitle("Open XML/SCV/Class file");
 
         Stage stage= (Stage) PaneID.getScene().getWindow();
 
-        fileChooser.getExtensionFilters().addAll((new FileChooser.ExtensionFilter("Xml File","*.xml")));
+        fileChooser.getExtensionFilters().addAll((new FileChooser.ExtensionFilter("XML File","*.xml")));
+
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV File","*.CSV"));
+
 
         File xmlFile = fileChooser.showOpenDialog(stage);
 
         if (xmlFile!=null)  // what to do if the file opened successfully
         {
-            Desktop desktop = Desktop.getDesktop();
+            //Desktop desktop = Desktop.getDesktop();  //opens the file
 
-            desktop.open(xmlFile);
+            //desktop.open(xmlFile);  //opens the file
+
+            successAlert();
         }
 
 
+
     }
+
+    void successAlert(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setTitle("Success");
+
+        alert.setHeaderText("File uploaded successfully");
+
+        alert.setContentText(null);
+
+        alert.showAndWait();
+    }
+
+    void failAlert(){
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setTitle("Failed");
+
+        alert.setHeaderText("File upload failed, please choose a valid XML file");
+
+        alert.setContentText(null);
+
+        alert.showAndWait();
+
+    }
+
+
+
+
 }
