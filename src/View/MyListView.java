993ff03902545081d209f38013649507c69026d4
kfirbilu <kfirbilu@gmail.com>
2021-05-05 16:46:03 +0300

    added MyListView

---
+++ src/View/MyListView.java
@@ -0,0 +1,46 @@
+package View;
+
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyListView extends Pane {
+
+MyListView myListView;
+
+Button open;
+
+public List<Node> set(){
+
+    List<Node> returnedValue = new ArrayList<>();
+
+    myListView = new MyListView();
+
+    myListView.setPrefSize(163,362);
+
+    myListView.setLayoutX(25);
+
+    myListView.setLayoutY(43);
+
+    returnedValue.add(myListView);
+
+    open = new Button("Open");
+
+    open.setLayoutX(25);
+
+    open.setLayoutY(410);
+
+    open.setPrefSize(163,42);
+
+    returnedValue.add(open);
+
+    return returnedValue;
+
+        }
+
+}
