b5c6e6c9b967745e8b3a4d1e1a75d6fe61b69c0f
GlaiChen <Gleicher@cs.colman.ac.il>
2021-05-05 20:08:21 +0300

    Plugins

---
+++ src/Plugin/AlgoPluginLoaderImpl.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class AlgoPluginLoaderImpl implements PluginLoader<TimeSeriesAnomalyDetector> {
+
+    @Override
+    public TimeSeriesAnomalyDetector load(String path, String className) throws MalformedURLException, ClassNotFoundException, IllegalAccessException, InstantiationException, FileNotFoundException {
+
+        System.out.println("Class directory is: " + path);
+
+        System.out.println("Class name is: " + className);
+
+        // load class directory
+        //URLClassLoader urlClassLoader = URLClassLoader.newInstance(new URL[]{
+        //      new URL("file://" + path)
+        //});
+        File file = new File(path);
+
+
+        // Convert File to a URL
+        URL url = file.toURI().toURL();
+        URL[] urls = new URL[]{url};
+        URLClassLoader urlClassLoader = URLClassLoader.newInstance(urls);
+        FileInputStream objReader = new FileInputStream(path);
+        ClassLoader.getSystemResource();
+       ClassLoader.getSystemClassLoader().getResourceAsStream(objReader.toString());
+        Class<?> c = urlClassLoader.loadClass(className);
+
+        // return a TimeSeriesAnomalyDetector instance
+        return (TimeSeriesAnomalyDetector) c.newInstance();
+
+    }
+
+
+}
---
+++ src/Plugin/maintest.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+
+class test {
+
+    public static void main(String[] args) throws ClassNotFoundException, MalformedURLException, InstantiationException, IllegalAccessException, FileNotFoundException {
+
+        final String GIT_PATH = "https://github.com/kfirbilu/PTM2-Project/tree/main/src/Algorithms";
+        final String LOCAL_PATH = "C:\\Users\\97254\\Documents\\GitHub\\Temp\\Impl\\resources\\hybrid.class";
+        final String HYBID_ALOG = "hybrid";
+        final String CSV_PATH = "https://github.com/kfirbilu/PTM2-Project/tree/main/src/moudle files/anomaly_flight.csv";
+        final String LOCAL_CSV_PATH = "C:\\Users\\97254\\Documents\\GitHub\\PTM2-Project\\src\\moudle files\\anomaly_flight.csv";
+        PluginLoader<TimeSeriesAnomalyDetector> pluginLoader = new AlgoPluginLoaderImpl();
+        TimeSeriesAnomalyDetector anomalyDetector = pluginLoader.load(LOCAL_PATH,HYBID_ALOG);
+        anomalyDetector.detect(new TimeSeries(LOCAL_CSV_PATH));
+        //anomalyDetector.detect()
+    }
+}
